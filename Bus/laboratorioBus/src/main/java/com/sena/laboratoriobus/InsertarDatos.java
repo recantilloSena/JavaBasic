10eb5af0166a3fe7a1ebcf730b57d427c2bbf2d4
Ricardo Enrique Cantillo Carrillo <recantillo1@misena.edu.co>
2024-08-20 20:00:51 -0500

    Menu Completo

--- Bus/laboratorioBus/src/main/java/com/sena/laboratoriobus/InsertarDatos.java
+++ Bus/laboratorioBus/src/main/java/com/sena/laboratoriobus/InsertarDatos.java
@@ -9,40 +9,76 @@ import java.util.Scanner;
 
 public class InsertarDatos {
     
+    
      public static void main(String[] args) throws SQLException, IOException  {
         
+         
          Scanner scanner = new Scanner(System.in);
          
          String nombre,nombreDeporte;
          Integer edad;
+         Integer idDeporte, opcion=6;
+         
+         do {      
+         System.out.println("MENU DE OPCIONES");
+         System.out.println("1. Crear Deporte");
+         System.out.println("2. Crear Persona");
+         System.out.println("3. Listar Personas");
+         System.out.println("4. Listar Desportes");
+         System.out.println("5. Salir");
+         System.out.println("--------------------");
+         opcion = scanner.nextInt();
+         
+             if (opcion==1){
+                crearDeporte();
+             }
+             if (opcion==2){
+                crearPersona();
+             }        
+         
+         
+             
+         } while ( opcion != 5  );
+         
+          
+         
+         
+         
+         
+         
+         
+         
+         
+         
+     }
+
+    private static void crearDeporte() throws SQLException, IOException {
+        Scanner scanner = new Scanner(System.in);
+        Controlador controlador = new Controlador();
+         String nombre,nombreDeporte;
+         Integer edad;
          Integer idDeporte;
          
-         System.out.println("Digite Nombre = ");
+         System.out.println("Digite Nombre de Deporte = ");
+         nombreDeporte = scanner.next();         
+         System.out.println("Digite el Id del Deporte= ");
+         idDeporte = scanner.nextInt();         
+         controlador.adicionarDeporte(idDeporte, nombreDeporte);    
+    }
+
+    private static void crearPersona() throws SQLException, IOException {System.out.println("Digite Nombre = ");
+        Scanner scanner = new Scanner(System.in);
+        Controlador controlador = new Controlador();
+        String nombre,nombreDeporte;
+         Integer edad;
+         Integer idDeporte;
          nombre = scanner.next();
          
          System.out.println("Digite La edad de la persona= ");
          edad = scanner.nextInt();
          
          System.out.println("Digite El Código del Deporte = ");
-         idDeporte = scanner.nextInt();
-         
-         
-         
-         Controlador controlador = new Controlador();
-         
-         controlador.adicionarPersona(nombre, edad, idDeporte);
-         
-         System.out.println("Digite Nombre de Deporte = ");
-         nombreDeporte = scanner.next();
-         
-         System.out.println("Digite el Id del Deporte= ");
-         idDeporte = scanner.nextInt();
-         
-         controlador.adicionarDeporte(idDeporte, nombreDeporte);
-         
-         
-         
-         
-     }
+         idDeporte = scanner.nextInt();    
+         controlador.adicionarPersona(nombre, edad, idDeporte);    }
     
 }
